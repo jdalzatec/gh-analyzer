56463340c5ce531bd4f265c24c10d5d89ddcc454
Juan David Alzate Cardona <jdalzatec@unal.edu.co>
2025-05-25 20:25:56 -0500

    Implementing queue as stream

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+from asyncio import Semaphore, Queue
 from aiohttp import ClientSession
 from datetime import datetime
 from pydantic import BaseModel
@@ -9,27 +10,30 @@ class RepoData(BaseModel):
     forks_count: int
     updated_at: datetime
 
-async def fetch_repo_data(session: ClientSession, repo_name: str) -> RepoData:
-    url = f"https://api.github.com/repos/{repo_name}"
-    headers = {
-        "Accept": "application/vnd.github+json",
-        "X-GitHub-Api-Version": "2022-11-28"
-    }
-    if repo_name == "tiangolo/fastapi":
-        await asyncio.sleep(10)
+async def fetch_repo_data(session: ClientSession, semaphore: Semaphore, queue: Queue, repo_name: str) -> RepoData:
+    async with semaphore:
+        print(f"Fetching data for {repo_name}...")
+        url = f"https://api.github.com/repos/{repo_name}"
+        headers = {
+            "Accept": "application/vnd.github+json",
+            "X-GitHub-Api-Version": "2022-11-28"
+        }
+        if repo_name == "tiangolo/fastapi":
+            await asyncio.sleep(10)
 
-    async with session.get(url, headers=headers) as response:
-        if response.status == 200:
-            data = await response.json()
-            return RepoData(
-                full_name=data["full_name"],
-                stargazers_count=data["stargazers_count"],
-                forks_count=data["forks_count"],
-                updated_at=data["updated_at"]
-            )
-        else:
-            print(f"Failed to fetch data for {repo_name}: {response.status}")
-            return None
+        async with session.get(url, headers=headers) as response:
+            if response.status == 200:
+                data = await response.json()
+                repo_data = RepoData(
+                    full_name=data["full_name"],
+                    stargazers_count=data["stargazers_count"],
+                    forks_count=data["forks_count"],
+                    updated_at=data["updated_at"]
+                )
+                await queue.put(repo_data)
+            else:
+                print(f"Failed to fetch data for {repo_name}: {response.status}")
+                return None
 
 
 
@@ -40,10 +44,12 @@ async def main():
         "pallets/flask",
     ]
     print('Analyzing repositories...')
+    semaphore = Semaphore(1)
+    queue = Queue()
     async with ClientSession() as session:
-        tasks = (fetch_repo_data(session, repo) for repo in repos)
+        tasks = (fetch_repo_data(session, semaphore, queue, repo) for repo in repos)
         for result in asyncio.as_completed(tasks):
             repo_data = await result
-            print(repo_data)
+            print(queue.qsize())
 
 asyncio.run(main())
